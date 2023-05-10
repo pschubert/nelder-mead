9bcd6612ca54e5821b53de78b49d6e8e19a2efe1
Philipp Schubert <philipp.schubert@bi.mpg.de>
2023-05-10 20:48:03 +0200

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='nelder_mead',
+    name='nelder-mead',
     packages=find_packages(),
+    python_requires='>=3.7',
+    version='0.0.1',
 )
